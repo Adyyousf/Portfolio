96d6c6ddad753dc8c103e94f8f4eaa3bc422ed68
AdyYousf <adyyousf197@gmail.com>
2021-07-05 00:56:16 +0530

    used template inheritance ( added base.html )

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+# Django admin header customization
+
+admin.site.site_header = "Login to Developer Ady"
+admin.site.site_title = "Wlecome to Ady's Dashboard"
+admin.site.index_title = "Welcome to this portal"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('projects', views.projects, name='projects'),
     path('contact', views.contact, name='contact')
-
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
+
 
 # Create your views here.
 def home(request):
@@ -15,6 +17,17 @@ def projects(request):
     return render(request, 'projects.html')
 
 def contact(request):
+    if request.method=="POST":
+
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        desc = request.POST['desc']
+        # print(name, email, phone, desc)
+        ins = Contact(name=name, email=email, phone=phone, desc=desc)
+        ins.save()
+        print("The data has been written to the DB")
+
     # return HttpResponse("This is my Contact page (/contact)")
     return render(request, 'contact.html')
 
